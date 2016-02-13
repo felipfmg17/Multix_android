aca983c0b97e7b045f96bac1158ea32b83cf287a
FelipeMendoza <felipfmg17@gmail.com>
2016-02-12 18:21:19 -0600

    Boton consultar funcionando bien :) :) , falta arrancar servicio, falta mostrar placa default al lanzar la aplicion, falta menu y descripcion de la multa

--- Multix/app/src/main/java/felpo/multix/android/MainActivity.java
+++ Multix/app/src/main/java/felpo/multix/android/MainActivity.java
@@ -1,5 +1,6 @@
 package felpo.multix.android;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +11,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import felpo.multix.R;
+import felpo.multix.core.History;
 import felpo.multix.core.Multix;
 
 public class MainActivity extends ActionBarActivity {
@@ -20,11 +24,15 @@ public class MainActivity extends ActionBarActivity {
     private ListView vMultas;
     private TextView vNoMultas;
     private String placa;
+    private History history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        common = new Common(this);
         setContentView(R.layout.activity_main);
+        findViews();
+        showInitialGui();
     }
 
 
@@ -52,16 +60,42 @@ public class MainActivity extends ActionBarActivity {
 
 
     public void onClickConsultar(View v){
-
+        consultarMultas();
     }
 
 
 
-    private void consultarMultas(){
+    private void startWaitService(){
+        startService(new Intent(this,WaitService.class));
+    }
 
+    private void consultarMultas(){
+        extractPlaca();
+        if(validatePlacaSize()){
+            Thread thread = new Thread(new RunRequestMultasConsultar());
+            thread.start();
+        }else{
+            showEmptyGui();
+        }
+
+    }
+
+    private void extractPlaca(){
+        placa = vPlaca.getText().toString().toUpperCase();
     }
 
     private void showMultasOnGui(){
+        if(history.multas.size()>0){
+            MultaListAdapter adapter = new MultaListAdapter(history.multas, this);
+            vMultas.setAdapter(adapter);
+            vNoMultas.setVisibility(View.INVISIBLE);
+            vMultas.setVisibility(View.VISIBLE);
+
+        }else{
+            vMultas.setVisibility((View.INVISIBLE));
+            vNoMultas.setText(getString(R.string.no_tienes_multas));
+            vNoMultas.setVisibility(View.VISIBLE);
+        }
 
     }
 
@@ -96,6 +130,10 @@ public class MainActivity extends ActionBarActivity {
         ATool.toast(this,placa+": "+getString(R.string.placa_default_confirmation));
     }
 
+    private void showInitialGui(){
+        showEmptyGui();
+    }
+
     private void launchMultaDescription(){
 
     }
@@ -103,11 +141,69 @@ public class MainActivity extends ActionBarActivity {
     private boolean validatePlacaSize(){
         if(placa!=null){
             int size = placa.length();
-            if(size >= Multix.MIN_PLACA_SIZE){
-                showPlacaSizeMessageError();
+            if(size >= Multix.MIN_PLACA_SIZE)
                 return true;
-            }
+            showPlacaSizeMessageError();
         }
         return false;
+    }
+
+    private void requestMultasConsultar(){
+        try {
+            history = Multix.requestHistory(placa);
+            runOnUiThread(new RunShowMultasOnGui());
+            ATool.d(" "+common.defaultPlacaExists());
+            if(!common.defaultPlacaExists()){
+                common.setDefaultPlaca(placa);
+                runOnUiThread(new RunShowDefaultPlacaConfirmationMessage());
+                common.saveOldHistory(history);
+            }
+            else if(common.getDefaultPlaca().equals(placa)){
+                common.saveOldHistory(history);
+            }
+        } catch (IOException e) {
+            runOnUiThread(new RunShowErrorMessage());
+            runOnUiThread(new RunShowEmptyGui());
+            e.printStackTrace();
+        }
+    }
+
+    private class RunRequestMultasConsultar implements Runnable{
+        @Override
+        public void run() {
+            requestMultasConsultar();
+        }
+    }
+
+    private class RunShowMultasOnGui implements Runnable {
+
+        @Override
+        public void run() {
+            showMultasOnGui();
+        }
+    }
+
+    private class RunShowErrorMessage implements Runnable {
+
+        @Override
+        public void run() {
+            showErrorMessage();
+        }
+    }
+
+    private class RunShowDefaultPlacaConfirmationMessage implements  Runnable{
+
+        @Override
+        public void run() {
+            showDefaultPlacaConfirmationMessage();
+        }
+    }
+
+    private class RunShowEmptyGui implements Runnable{
+
+        @Override
+        public void run() {
+            showEmptyGui();
+        }
     }
 }
--- Multix/app/src/main/java/felpo/multix/android/MultaListAdapter.java
+++ Multix/app/src/main/java/felpo/multix/android/MultaListAdapter.java
@@ -20,6 +20,8 @@ public class MultaListAdapter extends BaseAdapter{
     private Activity context;
     private LayoutInflater layoutInflater;
 
+    private static final int MOTIVO_TEXT_LENGTH = 40;
+
 
     public MultaListAdapter(List<Multa> multas, Activity context) {
         this.multas = multas;
@@ -47,23 +49,23 @@ public class MultaListAdapter extends BaseAdapter{
         View view = convertView;
         ViewHandler h = null;
         if(view==null){
-            view = layoutInflater.inflate(R.layout.multa_item_layout,parent);
+            view = layoutInflater.inflate(R.layout.multa_item_layout,null);
             h = new ViewHandler(view);
+            view.setTag(h);
         }
-        else
-            h = (ViewHandler) view.getTag();
 
+        h = (ViewHandler) view.getTag();
         fillView(h, position);
         return view;
     }
 
     private void fillView(ViewHandler h, int position){
         h.vFecha.setText(multas.get(position).fecha);
-        h.vMotivo.setText(multas.get(position).motivo);
-        h.vStatus.setText(multas.get(position).status);
+        h.vMotivo.setText(multas.get(position).motivo.substring(0,MOTIVO_TEXT_LENGTH));
+        h.vStatus.setText(multas.get(position).status.toUpperCase());
 
         if(multas.get(position).status.equals(Multix.PAGADA)){
-            h.vStatus.setTextColor(Color.GREEN);
+            h.vStatus.setTextColor(Color.argb(255,0,200,0));
         }else{
             h.vStatus.setTextColor(Color.RED);
         }
